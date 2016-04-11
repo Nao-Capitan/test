6a1f0c6bb7ef8069510a0da5f96c15f39fa1134d
nao-capitan <naohiro_capitan@kxa.biglobe.ne.jp>
2016-04-12 00:01:05 +0900

    Update

--- arrownode.js
+++ arrownode.js
@@ -1,20 +1,22 @@
 createsvg();
 
 function createsvg () {
-  // id:exampleが指定されているタグ(ここではdivタグ)の下に、svgを追加します。
-  // widthとheightを指定します。
   var svg = d3.select("#example").append("svg")
       .attr({
         width: 640,
-        height: 480,
+        height: 480
       });
 
-  // svgの下にcircleを追加します。
-  // cx,cy:中心座標(x,y)、r:半径を指定します。
-  svg.append('circle')
+  // 座標(cx,cy)と半径(r)を指定
+  var c1 = [100, 90, 30];
+
+  // dataの挿入方法が独特なので注意が必要
+  // 詳しくは、[三つの小円](http://ja.d3js.node.ws/document/tutorial/circle.html)参照
+  var circle = svg.selectAll('circle').data([c1]).enter().append('circle')
     .attr({
-      'cx': 100,
-      'cy': 90,
-      'r': 20,
+      // enterに入っているデータ一つ一つで下の処理を行う
+      'cx': function(d) { return d[0]; },
+      'cy': function(d) { return d[1]; },
+      'r': function(d) { return d[2]; },
     });
 };
