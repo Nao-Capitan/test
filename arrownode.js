36b90ce841df87e5b052ffae83c3c80c47c32cc5
nao-capitan <naohiro_capitan@kxa.biglobe.ne.jp>
2016-04-12 00:19:35 +0900

    色を付ける

--- arrownode.js
+++ arrownode.js
@@ -7,19 +7,19 @@ function createsvg () {
         height: 480
       });
 
-  // 指定を２つに
   var c1 = [100, 90, 30];
   var c2 = [200, 120, 20];
-  var c3 = [200, 310, 20];
+  var carray = [c1, c2];
 
-  // 指定した値を配列にする
-  var carray = [c1, c2, c3];
-
-  // dataに上で作成した配列を入れる
+  // 10種類の色を返す関数を使う
+  var color = d3.scale.category10();
   var circle = svg.selectAll('circle').data(carray).enter().append('circle')
     .attr({
       'cx': function(d) { return d[0]; },
       'cy': function(d) { return d[1]; },
       'r': function(d) { return d[2]; },
+      // dはデータ要素そのもの、iはindex番号を返す
+      // color(i)で、n番目の色データを返す
+      'fill': function(d,i) { return color(i); },
     });
 };
