a92e2bb546b0a8ef77de9292cd16e45c74019d2d
nao-capitan <naohiro_capitan@kxa.biglobe.ne.jp>
2016-04-12 00:02:37 +0900

    円を２つに

--- arrownode.js
+++ arrownode.js
@@ -7,14 +7,16 @@ function createsvg () {
         height: 480
       });
 
-  // 座標(cx,cy)と半径(r)を指定
+  // 指定を２つに
   var c1 = [100, 90, 30];
+  var c2 = [200, 120, 20];
 
-  // dataの挿入方法が独特なので注意が必要
-  // 詳しくは、[三つの小円](http://ja.d3js.node.ws/document/tutorial/circle.html)参照
-  var circle = svg.selectAll('circle').data([c1]).enter().append('circle')
+  // 指定した値を配列にする
+  var carray = [c1, c2];
+
+  // dataに上で作成した配列を入れる
+  var circle = svg.selectAll('circle').data(carray).enter().append('circle')
     .attr({
-      // enterに入っているデータ一つ一つで下の処理を行う
       'cx': function(d) { return d[0]; },
       'cy': function(d) { return d[1]; },
       'r': function(d) { return d[2]; },
